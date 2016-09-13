6c88bb80faee2dc81b9b2153359705225642aabd
renboqie <470371045@qq.com>
2016-09-14 01:35:38 +0800

    增删改查

--- src/win/renboqieqie/dao/LuceneDao.java
+++ src/win/renboqieqie/dao/LuceneDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
@@ -32,21 +33,48 @@ public class LuceneDao {
 	@Test
 	public void addIndex(Article article) throws IOException
 	{
+		
 		IndexWriter indexWriter = LuceneUtil.getIndexWriter();
 		Document document = ArticleUtils.articleToDocument(article);
 		indexWriter.addDocument(document);
 		indexWriter.close();
 	}
-	public void delIndex() throws IOException
+	/**
+	 * 删除索引，根据字段删除索引
+	 * @param fielName
+	 * @param fieldValue
+	 * @throws IOException
+	 */
+	public void delIndex(String fielName,String fieldValue) throws IOException
 	{
 		IndexWriter indexWriter = LuceneUtil.getIndexWriter();
 		
-	}
-	public void updateIndex()
-	{
+		Term term = new Term(fielName,fieldValue);
+		//delete from table where condition
+		indexWriter.deleteDocuments(term);
 		
 	}
-	public List<Article> findIndex(String  keywords) throws Exception
+	/**
+	 * 先删除复合条件的，在添加一条
+	 * @param fielName
+	 * @param fieldValue
+	 * @param article
+	 * @throws Exception
+	 */
+	public void updateIndex(String fielName,String fieldValue,Article article) throws Exception
+	{
+		IndexWriter indexWriter = LuceneUtil.getIndexWriter();
+		Term term = new Term(fielName,fieldValue);
+		Document doc = ArticleUtils.articleToDocument(article);
+		/**
+		 * 1.设置更新的条件
+		 * 
+		 * 2.设置更新的内容
+		 * 
+		 */
+		indexWriter.updateDocument(term, doc);;
+	}
+	public List<Article> findIndex(String  keywords,int start,int rows) throws Exception
 	{
 		IndexSearcher indexSearcher = LuceneUtil.getIndexSearch();
 		//需要根据那几个字段进行检索……
@@ -55,19 +83,24 @@ public class LuceneDao {
 		//不同的规则构造不同的子类……
 		//title:keywords , content:keywords
 		Query query = queryParser.parse(keywords);
-		TopDocs topDocs = indexSearcher.search(query, 100);
+		
+		//检索索引目录，会把索引目录都读取一边……
+		//检索复合query 前面N条记录……
+		TopDocs topDocs = indexSearcher.search(query, start+rows);
 		System.out.println("总记录数==total=="+topDocs.totalHits);
 		//唯一标识
 		ScoreDoc scoreDocs[] = topDocs.scoreDocs;
 		Article article = null;
 		List<Article> articlelist = new ArrayList<Article>();
-		for(int i=0 ;i<scoreDocs.length;i++){
+		//int endResult = start+rows>scoreDocs.length?scoreDocs.length:start+rows;
+		//在java中有一个类，比较连个值
+		int endResult= Math.min(start+rows, scoreDocs.length);
+		for(int i=start ;i<endResult;i++){
 			//docID LUCENE 为每一个document的唯一表示。
 			int docID= scoreDocs[i].doc;
 			System.out.println(docID);
 			article = new Article();
 			Document document = indexSearcher.doc(docID);
-			
 			article.setId(Integer.parseInt(document.get("id")));
 			article.setTitle(document.get("title"));
 			article.setAuthor(document.get("author"));
--- src/win/renboqieqie/test/JunitTest.java
+++ src/win/renboqieqie/test/JunitTest.java
@@ -13,22 +13,27 @@ public class JunitTest {
 	private LuceneDao luceneDao = new LuceneDao();
 	@Test
 	public void addIndex() throws IOException{
-		Article article = new Article();
-		article.setId(1);
 		
-		article.setTitle("第五百二十二章 巨大的爆炸");
-		article.setContent("巨大的爆炸，在瞬间爆出的热能就将这里一切的建筑都溶解掉了，电浆弹的使用，使得这个区域被破坏得相当彻底，半个爱情海的沙滩已经被高温之下彻底变成了晶体的平面，裸露出来的海滩下处处都是升腾的热气。");
-		article.setAuthor("跳舞");
-		article.setLink("http://www.sqsxs.com/book/2/2375/2571922.html");
-		luceneDao.addIndex(article);
+		for(int i=0;i<50;i++)
+		{
+			Article article = new Article();
+			
+			article.setId(i);
+			
+			article.setTitle("第五百二十二章 巨大的爆炸");
+			article.setContent("巨大的爆炸，在瞬间爆出的热能就将这里一切的建筑都溶解掉了，电浆弹的使用，使得这个区域被破坏得相当彻底，半个爱情海的沙滩已经被高温之下彻底变成了晶体的平面，裸露出来的海滩下处处都是升腾的热气。");
+			article.setAuthor("跳舞");
+			article.setLink("http://www.sqsxs.com/book/2/2375/2571922.html");
+			luceneDao.addIndex(article);
+		}
 	}
 	@Test
 	public void testSearch() throws Exception{
-		String keywords = "跳舞";
+		String keywords = "巨大";
 		//根据title和content去检索    textfield 现在使用得是单字分词器
 		//title：有
 		//title
-		List<Article> listArticles = luceneDao.findIndex(keywords);
+		List<Article> listArticles = luceneDao.findIndex(keywords,10,10);
 		for(Article article:listArticles)
 		{
 			System.out.println(article.getId());
@@ -37,5 +42,22 @@ public class JunitTest {
 			System.out.println(article.getContent());
 			System.out.println(article.getLink());
 		}
+	}
+	@Test
+	public void del() throws IOException{
+		luceneDao.delIndex("title", "跳舞");
+	}
+	@Test
+	public void testUpdate() throws Exception{
+		
+		String fielName= "title";
+		String fieldValue="巨";
+		Article article = new Article();
+		article.setAuthor("鸟");
+		article.setContent("巨大的鸟在飞");
+		article.setId(999);
+		article.setLink("www.baidu.com");
+		article.setTitle("巨大的鸟");
+		luceneDao.updateIndex(fielName, fieldValue,article);
 	}
 }
